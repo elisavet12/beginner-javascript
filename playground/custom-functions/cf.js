f5f14acca8df6a2ce9b244f3315ccd89e529f9b0
elisavet12 <85793200+elisavet12@users.noreply.github.com>
2021-11-06 20:14:17 +0200

    exercise!

--- playground/custom-functions/cf.js
+++ playground/custom-functions/cf.js
@@ -1,1 +1,25 @@
 console.log('it works');
+
+
+
+
+function calculateBill(billAmount, taxRate) {
+    console.log(billAmount, taxRate);
+    console.log("Running Calculate Bill!!!");
+    const total=billAmount*(1+taxRate);
+    return total;
+    
+}
+// const myTotal=calculateBill(100, 0.13);
+// const myTotal2=calculateBill(200, 0.13);
+// console.log(myTotal, myTotal2);
+
+const firstName="wes";
+
+function sayHiTo(){
+    return 'Hello ${firstName}';
+}
+
+const greeting = sayHiTo();
+console.log(greeting);
+
