47db65be43edbe9aefb3ccc18610df4b5983e51a
elisavet12 <85793200+elisavet12@users.noreply.github.com>
2021-11-12 19:49:28 +0200

    update

--- playground/custom-functions/cf.js
+++ playground/custom-functions/cf.js
@@ -3,23 +3,36 @@ console.log('it works');
 
 
 
-function calculateBill(billAmount, taxRate) {
+function calculateBill(billAmount, taxRate=0.13, tipRate=0.15) {
     console.log(billAmount, taxRate);
     console.log("Running Calculate Bill!!!");
-    const total=billAmount*(1+taxRate);
+    const total=billAmount+billAmount*taxRate+billAmount*tipRate;
     return total;
     
 }
-// const myTotal=calculateBill(100, 0.13);
-// const myTotal2=calculateBill(200, 0.13);
-// console.log(myTotal, myTotal2);
+const myBill4=calculateBill(100,undefined, 0.2);
+
+const myTotal=calculateBill(100, 0.13);
+const myTotal2=calculateBill(200, 0.13);
+
+console.log(myTotal, myTotal2);
 
 const firstName="wes";
 
 function sayHiTo(){
-    return 'Hello ${firstName}';
+    return `Hello ${firstName}`;
 }
 
-const greeting = sayHiTo();
-console.log(greeting);
+// const greeting = sayHiTo("wes");
+// console.log(greeting);
+// 
+
+function doctorize(name) {
+    return `Dr. ${name}`;
+}
+
+function yell(name="Silly Goose"){
+    return `HEY ${name.toUpperCase()}`;
+}
+doctorize ('wes')
 
---
+++ playground/custom-functions/ways-to-make-a-function.js
@@ -0,0 +1,17 @@
+console.log("it works");
+
+//anonymous function
+// function (firstName){
+//     return `Dr. ${firstName}`;
+// }
+
+//  function expression
+
+const doctorize = function (firstName) {
+  return `Dr. ${firstName}`;
+};
+
+function inchToCM(inches){
+    return inches*2.54;
+}
+
