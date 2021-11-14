d98b9ff5ca27b1e04b7fb1aca260cfb67b65349f
elisavet12 <85793200+elisavet12@users.noreply.github.com>
2021-11-14 19:01:40 +0200

    Update ways-to-make-a-function.js

--- playground/custom-functions/ways-to-make-a-function.js
+++ playground/custom-functions/ways-to-make-a-function.js
@@ -1,23 +1,74 @@
-console.log("it works");
+// console.log("it works");
 
-//anonymous function
-// function (firstName){
-//     return `Dr. ${firstName}`;
+// //anonymous function
+// // function (firstName){
+// //     return `Dr. ${firstName}`;
+// // }
+
+// //  function expression
+
+// const doctorize = function (firstName) {
+//   return `Dr. ${firstName}`;
 // }
 
-//  function expression
 
-const doctorize = function (firstName) {
-  return `Dr. ${firstName}`;
-}
+// /* eslint-disable */
+// const inchToCM = (inches) => inches * 2.54;
+
+// // function add(a, b=3){
+// // const total=a+b;
+// // return total;
+// // }
+
+// const add=(a,b=3)=>a+b;
+
+// // function makeABady(first, last){
+// //   const baby={
+// //   name:`${first} ${last}`,
+// //   age:0,
+// // };
+
+// // return baby;
+// // }
 
 
-/* eslint-disable */
-const inchToCM = (inches) => inches * 2.54;
 
-function add(a, b=3){
-const total=a+b;
-return total;
-}
+// function makeABaby(first, last) {
+//   const baby = {
+//     name: `${first} ${last}`,
+//     age: 0,
+//   };
+//   return baby;
+// }
 
-// const add(a,b=3)=>a+b;
+// const makeABady=(first, last) => ({name: `${first} ${last}`, age: 0});
+
+
+(function (age) {
+  console.log("Running the Anon function");
+  return `Your are cool and ${age}`;
+})(age);
+
+// const wes = {
+//   name: function sayHi() {
+//     console.log("Hey wes");
+//     return "Hey Wes!";
+//   }
+// };
+
+
+
+function wes();
+
+const wes = {
+  name: "Wes Bos",
+  // Method!
+  sayHi: function sayHi() {
+    console.log("Hey Wes!");
+    return "Hey Wes!";
+  },
+  //Short hand Method
+  yellHi() {
+    console.log("HEY WESSSSS");
+  },
+};
