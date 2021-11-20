303620e7688bcd668c47053c2f4feb5b16e2bcb4
elisavet12 <85793200+elisavet12@users.noreply.github.com>
2021-11-20 17:02:17 +0200

    s

---
+++ playground/hoisting.js
@@ -0,0 +1,15 @@
+console.log(age);
+var age=10;
+
+console.log('It works');
+
+sayHi();
+function sayHi(){
+    console.log('hey')
+    console.log(add(10,2));
+}
+
+function add(a,b){
+    return a+b;
+}
+
