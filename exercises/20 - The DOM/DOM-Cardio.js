f79c5ccdc395dc56ad7292f90fe9d721d380f7b0
elisavet12 <85793200+elisavet12@users.noreply.github.com>
2021-12-25 22:03:36 +0200

    hello

--- exercises/20 - The DOM/DOM-Cardio.js
+++ exercises/20 - The DOM/DOM-Cardio.js
@@ -1,27 +1,72 @@
+// const { Linter } = require("eslint");
+
 // Make a div
+const div=document.createElement('div');
 
 // add a class of wrapper to it
-
+div.classList.add('wrapper');
 // put it into the body
 
+document.body.appendChild(div);
+
+
 // make an unordered list
+const ul= `<ul>
+<li>one</li>
+<li>two</li>
+<li>three</li>
+</ul>`;
 
 // add three list items with the words "one, two, three" in them
+
 // put that list into the above wrapper
+
+div.innerHTML=ul;
+console.log(div);
 
 // create an image
 
+const img=document.createElement('img');
+
 // set the source to an image
+
+img.src='https://picsum.photos/500';
 // set the width to 250
+img.width=250;
+img.height=250;
 // add a class of cute
+img.classList.add('cute');
+
 // add an alt of Cute Puppy
+img.alt="Cute Puppy";
+
 // Append that image to the wrapper
 
+div.appendChild(img);
+
 // with HTML string, make a div, with two paragraphs inside of it
+const myHTML= `
+<div class="myDiv">
+<p>Paragraph One</p>
+<p>Paragraph Two</p>
+</div>`;
+
 // put this div before the unordered list from above
 
+const ulElement=div.querySelector('ul');
+console.log(ulElement);
+
+ulElement.insertAdjacentHTML('beforebegin', myHTML);
+
 // add a class to the second paragraph called warning
+
+const myDiv=document.querySelector('.myDiv');
+console.log(myDiv.children[1]);
+myDiv.children[1].classList.add('warning');
+
 // remove the first paragraph
+
+myDiv.firstElementChild.remove();
 
 // create a function called generatePlayerCard that takes in three arguments: name, age, and height
 
--- exercises/20 - The DOM/creating-with-strings.js
+++ exercises/20 - The DOM/creating-with-strings.js
@@ -1,9 +1,11 @@
+console.log('it works');
 const item=document.querySelector('.item');
-item.innerHTML=`<h1>Hey How are you?</h1>`;
-console.log('item.innerHTML');
+// item.innerHTML=`<h1>Hey How are you?</h1>`;
+// console.log(item.innerHTML);
 
-const src = `https://picsum.photos/200`;
-const desc = "Cute pup";
+const width = 500;
+const src = `https://picsum.photos/${width}`;
+const desc = `Cute pup <img onload="alert('hacked');" src="https://picsum.photos/50"/>`;
 const myHTML = `
   <div class="wrapper">
     <h2>Cute ${desc}</h2>
@@ -11,5 +13,20 @@ const myHTML = `
   </div>
 `;
 
+
+
 item.innerHTML = myHTML;
-console.log(item.innerHTML);
+// console.log(item.innerHTML);
+
+// console.log(typeof myHTML);
+
+// // myHTML.classList.add('special');
+
+// const itemImage = document.querySelector('.wrapper img');
+// itemImage.classList.add('round');
+
+const myFragment=document.createRange().createContextualFragment(myHTML);
+
+console.log(myFragment.querySelector('img'));
+document.body.appendChild(myFragment);
+console.log(myFragment);
