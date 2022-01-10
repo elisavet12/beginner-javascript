c2aef25c908c505641f8799354fe611e30193a51
elisavet12 <85793200+elisavet12@users.noreply.github.com>
2022-01-10 19:35:44 +0200

    to be continued

--- exercises/36 - Tabs/tabs.js
+++ exercises/36 - Tabs/tabs.js
@@ -1,1 +1,19 @@
 console.log('ya ya wes we get it.. IT WORKS!');
+
+const tabs = document.querySelector('.tabs');
+const tabsButtons = document.querySelectorAll(`[role="tab"]`);
+const tabsPanels = document.querySelectorAll(`[role="tabpanel"]`);
+
+function handleTabClick(event){
+    // hide all tab panels
+  // mark all tabs as unselected
+  // mark the clicked tab as selected
+  // find the associated tabPanel and show it!
+  console.log(tabsPanels);
+}
+
+tabsButtons.forEach(button => button.addEventListener('click', handleTabClick));
+
+tabsPanels.forEach(panel => {
+    panel.hidden = true;
+});
