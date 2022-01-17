e1977a3db9de561896993edb708aac77d26c02ee
elisavet12 <85793200+elisavet12@users.noreply.github.com>
2022-01-17 21:10:14 +0200

    ..

--- exercises/36 - Tabs/tabs.js
+++ exercises/36 - Tabs/tabs.js
@@ -2,18 +2,27 @@ console.log('ya ya wes we get it.. IT WORKS!');
 
 const tabs = document.querySelector('.tabs');
 const tabsButtons = document.querySelectorAll(`[role="tab"]`);
-const tabsPanels = document.querySelectorAll(`[role="tabpanel"]`);
+const tabPanels = document.querySelectorAll(`[role="tabpanel"]`);
 
 function handleTabClick(event){
     // hide all tab panels
+    tabPanels.forEach(panel => {
+  panel.hidden = true;
+});
   // mark all tabs as unselected
+  tabsButtons.forEach(tab => {
+    tab.setAttribute('aria-selected', false);
+  });
   // mark the clicked tab as selected
+  event.currentTarget.setAttribute('aria-selected', true);
   // find the associated tabPanel and show it!
-  console.log(tabsPanels);
+  const id = event.currentTarget.id;
+  const tabPanels = tabs.querySelector(`[aria-labelledby="${id}"]`);
+console.log(tabPanels);
+tabPanels.hidden = false;
+  
+  console.log(event.currentTarget);
 }
 
 tabsButtons.forEach(button => button.addEventListener('click', handleTabClick));
 
-tabsPanels.forEach(panel => {
-    panel.hidden = true;
-});
